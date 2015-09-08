fad5cca645e8dae6add026fe9e25300dd4891a46
TJ Ferry <tjtech1014@gmail.com>
2015-09-08 16:30:19 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # coding: utf-8
-feed = 'https://forum.omz-software.com/category/5.rss'
+feed_1 = 'https://forum.omz-software.com/category/5.rss'
+feed_2 = 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
 twitter_mode = True
